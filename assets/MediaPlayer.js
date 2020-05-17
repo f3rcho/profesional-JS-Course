a4753360408a9674a44702890b5c8c4a8629a4f7
Fernando Cordero <fernan942@gmail.com>
2020-05-16 20:16:49 -0400

    Inheritance and the prototyoe chains

---
+++ assets/MediaPlayer.js
@@ -0,0 +1,32 @@
+function MediaPlayer(config) {
+    this.media = config.el;
+    this.plugins = config.plugins || [];
+
+    //mediaplayer method
+    this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+        plugin.run(this)
+    })
+}
+
+MediaPlayer.prototype.play = function() {
+    this.media.play()
+}
+MediaPlayer.prototype.pause = function() {
+    this.media.pause()
+}
+
+MediaPlayer.prototype.togglePlay = function () {
+    console.log(this.media.paused);
+    (this.media.paused) ? this.play() : this.pause()
+}
+MediaPlayer.prototype.mute = function () {
+    this.media.muted = true;
+}
+MediaPlayer.prototype.unmute = function () {
+    this.media.muted = false;
+}
+export default MediaPlayer
---
+++ assets/plugins/AutoPlay.js
@@ -0,0 +1,6 @@
+function Autoplay() {}
+Autoplay.prototype.run = function(player) {
+    player.mute()
+    player.play()
+}
+export default Autoplay

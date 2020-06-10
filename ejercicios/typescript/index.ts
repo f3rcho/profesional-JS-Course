ab8045ac26ff004b9af31e998465850de1ba5879
Fernando Cordero <fernan942@gmail.com>
2020-06-10 07:44:32 -0400

    Interfaces 27

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -16,43 +16,77 @@ peopleAndNumbers.push("Ricardo");
 peopleAndNumbers.push(9001);
 
 // Enum
- enum Color {
+//  enum Color {
 
-    red = "red",
-    green = "green",
-    blue = "blue",
- }
+//     red = "red",
+//     green = "green",
+//     blue = "blue",
+//  }
 
-let colorFavorito: Color = Color.red
-console.log(`My fav color is ${colorFavorito}`);
+// let colorFavorito: Color = Color.red
+// console.log(`My fav color is ${colorFavorito}`);
 
-// Any
-let comodin: any = 'Joker';
-comodin = { type: 'Wildcard' }
+// // Any
+// let comodin: any = 'Joker';
+// comodin = { type: 'Wildcard' }
 
-// Object
-let someObject: Object = { type: 'Wildcard' }
+// // Object
+// let someObject: Object = { type: 'Wildcard' }
 
-// Functions
-function add(a: number, b: number): number {
-    return a + b;
+// // Functions
+// function add(a: number, b: number): number {
+//     return a + b;
+// }
+
+// const sum = add(4, 6)
+
+// // function inside other function
+// function createAdder(a: number): (number) => number {
+//     return function (b: number) {
+//         return b + a;
+//     };
+// }
+
+// const addFour = createAdder(4);
+// const fourPlus6 = addFour(6);
+
+// function fullName(firstName: string, lastName: string): string { 
+//     return `${firstName} ${lastName}`
+// }
+
+// const fernando = fullName('Fernando', 'Cordero')
+// console.log(fernando);
+
+// Interfaces define the exact form that have to have an object
+
+enum Color {
+    Red = "Red",
+    Green = "Green",
+}
+interface Rectangle {
+    width: number,
+    height: number,
+    color?: Color,
 }
 
-const sum = add(4, 6)
-
-// function inside other function
-function createAdder(a: number): (number) => number {
-    return function (b: number) {
-        return b + a;
-    };
+let rect: Rectangle = {
+    width: 4,
+    height: 6,
+    color: Color.Green,
 }
 
-const addFour = createAdder(4);
-const fourPlus6 = addFour(6);
-
-function fullName(firstName: string, lastName: string): string { 
-    return `${firstName} ${lastName}`
+function area(r: Rectangle): number {
+    return r.width * r.height
 }
+const areaRect = area(rect);
+console.log(areaRect);
 
-const fernando = fullName('Fernando', 'Cordero')
-console.log(fernando);
+// console.log(rect.toString());
+
+rect.toString = function() {
+    return this.color ? `A rectangle ${this.color}` : `A rectangle`
+}
+console.log(rect.toString());
+
+
+
